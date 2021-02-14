776c94e72554a1488f86205e338a93fd5b77853f
MorenoAlexander <am@alexandermoreno.com>
2021-02-14 15:41:59 -0600

    Added client-side files, docker files

--- clientapp/src/pages/About.vue
+++ clientapp/src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>This page will contain information about the system over all. Quick notes on the current systems in place, etc.</p>
   </Layout>
 </template>
 
