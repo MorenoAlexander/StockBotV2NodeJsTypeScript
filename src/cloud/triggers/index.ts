fde39914fcdc33cb5158f6a45774ee86609ac78c
Alexander Moreno <am@alexandermoreno.com>
2022-09-28 04:59:22 +0000

    removed cloud folder

--- src/cloud/triggers/index.ts
+++
@@ -1,6 +0,0 @@
-import Parse from 'parse/node';
-import * as beforeSaves from './beforeSave';
-
-export default function RegisterControllers() {
-  Parse.Cloud.beforeSave('StockLot', beforeSaves.StockLot);
-}
