68dbe9fc6daacf14d8995aee1a70b3ce498c493e
morenoalexander <am@alexandermoreno.com>
2022-10-09 10:29:14 -0500

    cleaned up command interface

--- src/interfaces/common/command.ts
+++ src/interfaces/common/command.ts
@@ -1,5 +1,10 @@
-export default interface Command {
+import type { Message } from 'discord.js';
+
+type Command = {
   name: string;
   description: string;
-  execute: () => void;
-}
+  // eslint-disable-next-line no-unused-vars
+  execute: (args: Message) => void;
+};
+
+export default Command;
