aba3e48fdbb28925f84e11360d4b4bacb4aef6e6
morenoalexander <am@alexandermoreno.com>
2022-11-01 09:31:14 -0500

    replaced parse logic with prisma logic

--- src/controllers/StockController.ts
+++ src/controllers/StockController.ts
@@ -1,9 +1,11 @@
+import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-import Parse from 'parse/node';
+
+const prismaClient = new PrismaClient();
 
 export default function Register(app: any) {
   app.get('/api/stock/getusers', async (req: Request, res: Response) => {
-    const data = await new Parse.Query(Parse.User).limit(100).find();
+    const data = await prismaClient.user.findMany({ take: 100 });
 
     res.status(200).json(data);
   });
